9708c0f9e399591b5d74308aa1e26def4748280b
genizara <genizara@gmail.com>
2021-07-17 02:49:30 +0900

    210717

---
+++ algoStudy1000/baekjoon/b2588.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = str(input())
+c = list(b)
+print(a * int(c[2]))
+print(a * int(c[1]))
+print(a * int(c[0]))
+print(a * int(b))
---
+++ chodangiTest.py
@@ -0,0 +1,6 @@
+# 2021.07.14 186p
+
+sungadae = ['아람', '지선', '보람', '홍주', '민지', '리나', '즤희', '태원']
+print(len(sungadae))
+sungadae.append('정달')
+print(len(sungadae))
