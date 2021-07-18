1d8571794a69b163d355eeba7f70fe9b2c4e5230
genizara <genizara@gmail.com>
2021-07-19 01:31:37 +0900

    210718

--- chodangiTest.py
+++ chodangiTest.py
@@ -1,13 +1,8 @@
-# 2021.07.14 186p
-
-scores = [11, 22, 33, 44, 55]
-print(scores)
-ss = scores[1:3]
-print(scores)
-print(ss)
-print(scores[2:])
-print(scores[:4])
-kk = scores[:]
-scores[3] = 99
-print(kk)
-print(scores)
+# 2021.07.18 243p
+a_dict = {'z': 1, 'b': 2, 'c': 2, 'ac': 4}
+print(a_dict)
+a_sorted_dict = sorted(a_dict)
+print(a_sorted_dict)
+print(a_dict)
+print(type(a_sorted_dict))
+print(type(a_dict))
